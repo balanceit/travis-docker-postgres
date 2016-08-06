af742f3f38faefd5832c1acac757d6d89432d1f3
David <balance.it@gmail.com>
2016-08-07 00:37:56 +1200

    random commit message 19 error

--- run.sh
+++ run.sh
@@ -2,5 +2,5 @@
 echo ${PGHOST}
 echo `which psql`
 echo 'before first select, should have from_travis table'
-psql -c "select table_name from information_schema.tables where table_schema= 'public';" -U postgres -h ${PGHOST} -p 5432111 -d docker_test_developement --echo-all --echo-errors  --output=file
+psql -c "select table_name from information_schema.tables where table_schema= 'public';" -U pos -h ${PGHOST} -p 543 -d test_developement --echo-all --echo-errors  --output=file
 cat file
