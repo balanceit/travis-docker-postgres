749bd44b70fa99dd357720fd8cfc3268dfd15962
David <balance.it@gmail.com>
2016-08-06 21:24:04 +1200

    random commit message 10

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo ${PGHOST}
 psql -c "select table_name from information_schema.tables where table_schema= 'public';" -U postgres -h ${PGHOST} -p 5432 -d docker_test_developement
-psql -c "create table list (currenttime timestamp not null default current_timestamp);" -U postgres -h ${PGHOST} -p 5432 -d docker_test_developement
+psql -c "create table from_container (currenttime timestamp not null default current_timestamp);" -U postgres -h ${PGHOST} -p 5432 -d docker_test_developement
 psql -c "select table_name from information_schema.tables where table_schema= 'public';" -U postgres -h ${PGHOST} -p 5432 -d docker_test_developement
