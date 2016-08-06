8424bd9f9ef3c3e728a8a193c20eca77c87e3c75
David <balance.it@gmail.com>
2016-08-06 21:32:04 +1200

    random commit message 13

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 echo ${PGHOST}
+echo `which psql`
 echo 'before first select, should have from_travis table'
 psql -c "select table_name from information_schema.tables where table_schema= 'public';" -U postgres -h ${PGHOST} -p 5432 -d docker_test_developement > out
 echo 'before creation of from_container'
