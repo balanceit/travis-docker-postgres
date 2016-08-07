4ac3fae6cc982d223f9c00fb101a0cdb8d57d72d
David <balance.it@gmail.com>
2016-08-07 19:25:28 +1200

    909

--- run.sh
+++ run.sh
@@ -10,7 +10,7 @@ nmap 127.0.0.1
 # echo `which psql`
 # echo 'before first select, should have from_travis table'
 # psql --version
-psql -c "select 1;" -U postgres -h 172.17.42.1 -p 5432 -d docker_test_developement --echo-all --echo-errors
+psql -c "select 1;" -U postgres -h 172.17.42.1 -p 5432 -d docker_test_developement --no-password --echo-all --echo-errors
 # psql -c "select 1;" -U postgres -h 10.128.0.1 -p 5432 -d docker_test_developement --echo-all --echo-errors
 # psql -c "select 1;" -U postgres -h localhost -p 5432 -d docker_test_developement --echo-all --echo-errors
 # psql -c "select 1;" -U postgres -h 127.0.0.1 -p 5432 -d docker_test_developement --echo-all --echo-errors
