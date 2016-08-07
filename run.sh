3979196b8f5d5ffae41c9a1b6b9522cd2ad24911
David <balance.it@gmail.com>
2016-08-07 14:11:29 +1200

    random commit message 30

--- run.sh
+++ run.sh
@@ -2,6 +2,10 @@
 ifconfig
 echo ${PGHOST}
 echo `which psql`
+echo 'ping 172.17.42.1'
+ping -t 3 172.17.42.1
+echo 'ping 10.128.0.1'
+ping -t 3 10.128.0.1
 echo 'before first select, should have from_travis table'
 psql --version
 psql -c "select 1;" -U postgres -h 172.17.42.1 -p 5432 -d docker_test_developement --echo-all --echo-errors
