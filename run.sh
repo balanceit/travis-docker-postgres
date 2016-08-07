4cbc8e9e426eed5ed606348dc849b38c512f9368
David <balance.it@gmail.com>
2016-08-07 14:06:44 +1200

    random commit message 28

--- run.sh
+++ run.sh
@@ -4,6 +4,7 @@ echo ${PGHOST}
 echo `which psql`
 echo 'before first select, should have from_travis table'
 psql --version
+psql -c "select 1;" -U postgres -h 172.17.42.1 -p 5432 -d docker_test_developement --echo-all --echo-errors
 psql -c "select 1;" -U postgres -h 10.128.0.1 -p 5432 -d docker_test_developement --echo-all --echo-errors
 psql -c "select 1;" -U postgres -h localhost -p 5432 -d docker_test_developement --echo-all --echo-errors
 psql -c "select 1;" -U postgres -h 127.0.0.1 -p 5432 -d docker_test_developement --echo-all --echo-errors
